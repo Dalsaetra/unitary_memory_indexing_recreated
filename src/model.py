a561bb2a4c7da9dafccc729a7d83887e01671006
Mattis Dalsætra <mattisdo@uio.no>
2023-01-19 16:59:13 +0100

    Trained, tested and tried L2 reg.

--- src/model.py
+++ src/model.py
@@ -6,11 +6,15 @@ import torch.optim as optim
 
 
 class UMI(nn.Module):
-    def __init__(self,in_size,out_size):
+    def __init__(self,in_size,out_size,L2=False):
         super().__init__()
-        self.L1 = nn.Linear(in_size,out_size)
+        self.L1 = nn.Linear(in_size,out_size,bias=False)
 
-        self.optim = optim.Adam(params=self.parameters(),lr=1e-3)
+        if L2:
+            weight_decay = 0.01
+        else:
+            weight_decay = 0.0
+        self.optim = optim.Adam(params=self.parameters(),lr=1e-2,weight_decay=weight_decay) # Weight decay implements L2 reg.
         self.L = nn.CrossEntropyLoss()
 
     def forward(self,x):
@@ -18,9 +22,9 @@ class UMI(nn.Module):
         return F.softmax(x,dim=-1)
 
     def loss_fn(self,x,y_true):
-        y_hat = self(x)
+        y_hat = self(x.float())
         loss = self.L(y_hat,y_true)
-        return loss.item()
+        return loss
 
     def train_step(self,x,y_true):
         self.optim.zero_grad()
