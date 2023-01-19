ed95566131935fd12f93f2ed216312536e57ba8d
Mattis Dalsætra <mattisdo@uio.no>
2023-01-19 13:58:04 +0100

    Start of model design

---
+++ src/model.py
@@ -0,0 +1,14 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from torch.autograd import Variable
+import torch.optim as optim
+
+
+
+class UMI(nn.Module):
+    def __init__(self):
+        pass
+
+    def forward(self,x):
+        pass
