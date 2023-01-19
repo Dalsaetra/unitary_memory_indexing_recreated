edfe03824f8d8a2b557911c973746261d6989353
Mattis Dalsætra <mattisdo@uio.no>
2023-01-19 14:15:03 +0100

    Designed nn model with training methods as well

--- src/model.py
+++ src/model.py
@@ -7,8 +7,25 @@ import torch.optim as optim
 
 
 class UMI(nn.Module):
-    def __init__(self):
-        pass
+    def __init__(self,in_size,out_size):
+        super().__init__()
+        self.L = nn.CrossEntropyLoss()
+        self.optim = optim.Adam(params=self.parameters(),lr=1e-3)
+
+        self.L1 = nn.Linear(in_size,out_size)
 
     def forward(self,x):
-        pass
+        x = self.L1(x)
+        return F.softmax(x,dim=-1)
+
+    def loss_fn(self,x,y_true):
+        y_hat = self(x)
+        loss = self.L(y_hat,y_true)
+        return loss.item()
+
+    def train_step(self,x,y_true):
+        self.optim.zero_grad()
+        loss = self.loss_fn(x,y_true)
+        loss.backward()
+        self.optim.step()
+        return loss.item()
