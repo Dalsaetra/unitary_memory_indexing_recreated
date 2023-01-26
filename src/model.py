56976eb623cd3d4580294789cf4df467419c4565
Mattis Dalsætra <mattisdo@uio.no>
2023-01-26 13:38:13 +0100

    Implemented hard SVB

--- src/model.py
+++ src/model.py
@@ -4,6 +4,9 @@ import torch.nn.functional as F
 from torch.autograd import Variable
 import torch.optim as optim
 
+import numpy as np
+import scipy as sp
+
 
 class UMI(nn.Module):
     def __init__(self,in_size,out_size):
@@ -47,3 +50,22 @@ class UMI_L2(UMI):
         self.optim = optim.Adam(params=self.parameters(),lr=1e-2,weight_decay=decay)
         
 
+class UMI_SVB(UMI):
+    def __init__(self,in_size,out_size):
+        super().__init__(in_size,out_size)
+        torch.nn.init.orthogonal_(self.L1.weight, gain=1)
+
+    def SVB(self,eps=0.001):
+        old_weights = self.L1.weight.data.clone().numpy()
+        w_svd = sp.linalg.svd(old_weights,full_matrices=False)
+        U,s,V = w_svd[0],w_svd[1],w_svd[2]
+        for i in range(len(s)):
+            if s[i] > 1 + eps:
+                s[i] = 1 + eps
+            elif s[i] < 1/(1+eps):
+                s[i] = 1/(1+eps)
+            else:
+                pass
+        new_weights = U @ np.diag(s) @ V
+        new_weights = torch.tensor(new_weights,dtype=torch.float32)
+        self.L1.weight.data = new_weights
