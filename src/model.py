6b5278f28620239917a2758a9461d2e1add49701
Mattis Dalsætra <mattisdo@uio.no>
2023-01-27 14:08:41 +0100

    Implemented and trained on asymmetric data

--- src/model.py
+++ src/model.py
@@ -5,10 +5,10 @@ from torch.autograd import Variable
 import torch.optim as optim
 
 class UMI(nn.Module):
-    def __init__(self,in_size,out_size):
+    def __init__(self,in_size,out_size,lr=1e-2,bias=False):
         super().__init__()
-        self.L1 = nn.Linear(in_size,out_size,bias=False)
-        self.optim = optim.Adam(params=self.parameters(),lr=1e-2)
+        self.L1 = nn.Linear(in_size,out_size,bias=bias)
+        self.optim = optim.Adam(params=self.parameters(),lr=lr)
         self.L = nn.CrossEntropyLoss()
 
     def forward(self,x):
@@ -29,8 +29,8 @@ class UMI(nn.Module):
 
 
 class UMI_L1(UMI):
-    def __init__(self,in_size,out_size,decay=0.01):
-        super().__init__(in_size,out_size)
+    def __init__(self,in_size,out_size,decay=0.01,lr=1e-2,bias=False):
+        super().__init__(in_size,out_size,lr=lr,bias=bias)
         self.decay = decay
 
     def loss_fn(self,x,y_true):
@@ -41,14 +41,25 @@ class UMI_L1(UMI):
 
 
 class UMI_L2(UMI):
-    def __init__(self,in_size,out_size,decay=0.01):
-        super().__init__(in_size,out_size)
-        self.optim = optim.Adam(params=self.parameters(),lr=1e-2,weight_decay=decay) # Weight decay implements L2 reg.
+    def __init__(self,in_size,out_size,decay=0.01,built_in=True,lr=1e-2,bias=False):
+        super().__init__(in_size,out_size,lr=lr,bias=bias)
+        if built_in:
+            self.optim = optim.Adam(params=self.parameters(),lr=lr,weight_decay=decay) # Weight decay implements L2 reg.
+        else:
+            self.decay = decay
+            self.built_in = built_in
+
+    def loss_fn(self,x,y_true):
+        y_hat = self(x.float())
+        loss = self.L(y_hat,y_true)
+        if not self.built_in:
+            loss += (self.decay*self.L1.weight**2).sum() # L2 reg.
+        return loss
         
 
 class UMI_SVB(UMI):
-    def __init__(self,in_size,out_size):
-        super().__init__(in_size,out_size)
+    def __init__(self,in_size,out_size,lr=1e-2,bias=False):
+        super().__init__(in_size,out_size,lr=lr,bias=bias)
         torch.nn.init.orthogonal_(self.L1.weight, gain=1)
 
     def SVB(self,eps=0.001):
@@ -68,14 +79,14 @@ class UMI_SVB(UMI):
             else:
                 pass
         new_weights = U @ torch.diag(s) @ V # Reconstruct the matrix with the new singular values
-        new_weights = torch.tensor(new_weights,dtype=torch.float32)
+        # new_weights = torch.tensor(new_weights,dtype=torch.float32)
         self.L1.weight.data = new_weights # Update the weights for the model
         # self.L1.weight.data = torch.nn.parameter.Parameter()
         
 
 class UMI_SVB_soft(UMI):
-    def __init__(self,in_size,out_size,decay=0.01):
-        super().__init__(in_size,out_size)
+    def __init__(self,in_size,out_size,decay=0.01,lr=1e-2,bias=False):
+        super().__init__(in_size,out_size,lr=lr,bias=bias)
         torch.nn.init.orthogonal_(self.L1.weight, gain=1)
         self.decay = decay
 
@@ -90,3 +101,27 @@ class UMI_SVB_soft(UMI):
         return loss
 
     
+class UMI_big(nn.Module):
+    def __init__(self,in_size,out_size,hidden_size=100):
+        super().__init__()
+        self.L1 = nn.Linear(in_size,hidden_size,bias=False)
+        self.L2 = nn.Linear(hidden_size,out_size,bias=False)
+        self.optim = optim.Adam(params=self.parameters(),lr=1e-2)
+        self.L = nn.CrossEntropyLoss()
+
+    def forward(self,x):
+        x = F.relu(self.L1(x))
+        x = self.L2(x)
+        return F.softmax(x,dim=-1)
+
+    def loss_fn(self,x,y_true):
+        y_hat = self(x.float())
+        loss = self.L(y_hat,y_true)
+        return loss
+
+    def train_step(self,x,y_true):
+        self.optim.zero_grad()
+        loss = self.loss_fn(x,y_true)
+        loss.backward()
+        self.optim.step()
+        return loss.item()
