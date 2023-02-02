bbb79073d5aefb16787dc58e1d2b7d4cecae5561
Mattis Dalsætra <mattisdo@uio.no>
2023-02-02 15:30:04 +0100

    Added bigger model and changed to sigmoid

--- src/model.py
+++ src/model.py
@@ -115,16 +115,44 @@ class UMI_SVB_soft(UMI):
 
     
 class UMI_big(nn.Module):
-    def __init__(self,in_size,out_size,hidden_size=100):
+    def __init__(self,in_size,out_size,hidden_size=100,lr=1e-2,bias=False):
         super().__init__()
-        self.L1 = nn.Linear(in_size,hidden_size,bias=False)
-        self.L2 = nn.Linear(hidden_size,out_size,bias=False)
-        self.optim = optim.Adam(params=self.parameters(),lr=1e-2)
+        self.L1 = nn.Linear(in_size,hidden_size,bias=bias)
+        self.L2 = nn.Linear(hidden_size,out_size,bias=bias)
+        self.optim = optim.Adam(params=self.parameters(),lr=lr)
         self.L = nn.CrossEntropyLoss()
 
     def forward(self,x):
-        x = F.relu(self.L1(x))
+        x = F.sigmoid(self.L1(x))
         x = self.L2(x)
+        return F.softmax(x,dim=-1)
+
+    def loss_fn(self,x,y_true):
+        y_hat = self(x.float())
+        loss = self.L(y_hat,y_true)
+        return loss
+
+    def train_step(self,x,y_true):
+        self.optim.zero_grad()
+        loss = self.loss_fn(x,y_true)
+        loss.backward()
+        self.optim.step()
+        return loss.item()
+
+
+class UMI_bigger(nn.Module):
+    def __init__(self,in_size,out_size,hidden_size=100,lr=1e-2,bias=False):
+        super().__init__()
+        self.L1 = nn.Linear(in_size,hidden_size,bias=bias)
+        self.L2 = nn.Linear(hidden_size,hidden_size,bias=bias)
+        self.L3 = nn.Linear(hidden_size,out_size,bias=bias)
+        self.optim = optim.Adam(params=self.parameters(),lr=lr)
+        self.L = nn.CrossEntropyLoss()
+
+    def forward(self,x):
+        x = F.sigmoid(self.L1(x))
+        x = F.sigmoid(self.L2(x))
+        x = self.L3(x)
         return F.softmax(x,dim=-1)
 
     def loss_fn(self,x,y_true):
